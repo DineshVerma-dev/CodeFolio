b79bf18232c0e318b0f6086c541d277b8f1071e1
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-02-28 20:56:34 +0530

    started building codefolio

---
+++ src/data/resume.tsx
@@ -0,0 +1,7 @@
+export const DATA = {
+    name: "Dinesh verma",
+    discription :
+     "I am a web developer at the forefront of technology, always eager to learn and implement the newest trends to enhance user experiences",
+    summary:
+    " Hello! I am in [second year Engineering student](#) from [India (Mumbai)](#) ,Learning new cutting edge technology  , leveraging the latest technologies to build dynamic, responsive, and user-centric web applications ,My expertise includes working with [ javascript , MongoDB , Express , React , nodejs](#), "
+}
